ae368282f207e02c44de4127dee643e5b4a2359a
Md. Naim Chowdhury <naimmcduck@gmail.com>
2020-07-05 16:35:55 +0200

    x

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User; //IMPORTANT NOTE
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+
+
+public function uploadAvatar(Request $request){
+    if ($request->hasFile('image')) {
+        $filename=$request->image->getClientOriginalName();
+        $request->image->storeAs('image',$filename,'public');
+        auth()->user()->update(['avatar'=>$filename]);
+        return redirect()->back();
+    }
+    else {
+        return 'no image found';
+    }
+    // dd($request->hasfile('image'));
+}
+
+public function index()
+    {
+        $data = [
+            'name'=>'elon',
+            'email'=>'elon3@gmail.com',
+            'password'=>'password',
+ 
+        ];
+        // User::create($data);
+        // $user = new User();
+        // $user->name='naim';
+        // $user->email='naim@gmail.com';
+        // $user->password=bcrypt('password');
+        // $user->save();
+
+        $user = User::all();
+        return $user;
+
+        // User::where('id',6)->delete();
+        // User::where('id',7)-> update(['name'=>'mian']);
+
+        // dd($user);
+        // var_dump($user);
+        // DB::insert('insert i nto users (name,email,password) values (?,?,?)', 
+        // ['naim','naim@gmail.com','password']);
+
+        // DB:: update('update users set name= ? where id = ?',['mian',1]); 
+        
+        // $users=DB:: select('select * from users');
+        // return $users;
+
+        // DB::delete('delete from users where id=1');
+
+        return view('home');
+    }
+} 
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request; //THIS IS IMPORTANT
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return env('App_Name');
+    return view('welcome');
+});
+Route::get('/user', 'UserController@index');
+Route::post('/upload', 'UserController@uploadAvatar');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

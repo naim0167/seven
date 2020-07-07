47c248756ed97000270ad3a47c0583e941c973b4
Md. Naim Chowdhury <naimmcduck@gmail.com>
2020-07-07 13:33:27 +0200

    24

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,1 +1,12 @@
-All todos
+@extends('todos.layout')
+
+@section ('content')
+    <h1 class="text-2xl">All Your Todos: </h1>
+    <ul>
+        @foreach ($todos as $todo)
+        <li>
+            {{$todo->title}}
+        </li>
+        @endforeach
+    </ul>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/todos','TodoController@index');
 Route::get('/todos/create','TodoController@create');
 Route::post('/todos/create', 'TodoController@store');
 Route::get('/todos/edit', 'TodoController@edit');
+
 Route::get('/', function () {
     // return env('App_Name');
     return view('welcome');

005d07dec10c5ffb01985cfc99280a0a40289ee1
Md. Naim Chowdhury <naimmcduck@gmail.com>
2020-07-08 20:56:19 +0200

    42

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -7,14 +7,13 @@
             <span class="fas fa-arrow-left"></span>
         </a>
     </div>
-    <h1 class="text 2xl">Whtat next you need to-do</h1>
     <x-alert/>
     <form method="post" action="{{route('todo.store')}}" class="py-5">
     @csrf
     <div class="py-1">
         <input type="text" name="title" class="p-2 rounded border" placeholder="Title">
     </div>
-     <div class="py-1">
+    <div class="py-1">
         <textarea name="description" class="p-2 rounded border" placeholder="Description"></textarea>
     </div>
     <div class="py-2">
@@ -29,4 +28,5 @@
         <input type="submit" value="Create" class="p-2 border rounded-lg">
     </div>
     </form>
+    @livewire('counter')
 @endsection
